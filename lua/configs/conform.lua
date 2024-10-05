050a82ab7d98b826bad8f386587a69a29c80438c
naillizard <1164234+naillizard@users.noreply.github.com>
2024-10-06 03:23:33 +1300

    chore: use nixpkgs_fmt for nix

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,20 +1,20 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    nix = { "alejandra" }, -- "nixpkgs_fmt"
-    go = { "goimports", "gofmt" },
-    bicep = { "bicep" },
-    justfile = { "just" },
-    yaml = { "yamlfmt" },
-    css = { "prettier" },
-    -- html = { "prettier" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		nix = { "nixpkgs_fmt" }, -- "alejandra" "nixpkgs_fmt"
+		go = { "goimports", "gofmt" },
+		bicep = { "bicep" },
+		justfile = { "just" },
+		yaml = { "yamlfmt" },
+		css = { "prettier" },
+		-- html = { "prettier" },
+	},
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 }
 
 return options
