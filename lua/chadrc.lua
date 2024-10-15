dfb2a3120fe91ebe900a765c6a4c83342a11bf22
naillizard <1164234+naillizard@users.noreply.github.com>
2024-10-16 09:01:25 +1300

    chore(theme): use tokyodark theme

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,16 +1,16 @@
 -- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v2.5/lua/nvconfig.lua
 
----@type ChadrcConfig
+---@class ChadrcConfig
 local M = {}
 
 M.base46 = {
-  theme = "chadracula-evondev",
+	theme = "tokyodark",
 
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
 }
 
 return M
