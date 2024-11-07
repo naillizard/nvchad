25bc1d08fe711e6f8b402dfb7de645932730af96
naillizard <1164234+naillizard@users.noreply.github.com>
2024-11-08 10:02:02 +1300

    chore: map docker compose file to the correct filetype for lsp detection

--- lua/options.lua
+++ lua/options.lua
@@ -1,12 +1,19 @@
-require "nvchad.options"
+require("nvchad.options")
 
 -- add yours here!
 vim.g.dap_virtual_text = true -- enable debug adapter protocol (DAP)
 vim.opt.colorcolumn = "80"
 
-vim.cmd [[ autocmd BufNewFile, BufRead *.bicep set filetype=bicep ]]
+vim.cmd([[ autocmd BufNewFile, BufRead *.bicep set filetype=bicep ]])
 
-vim.cmd [[ autocmd BufNewFile, BufRead /dev/shm/gopass* set setlocal noswapfile nobackup noundofile shada="" ]]
+vim.cmd([[ autocmd BufNewFile, BufRead /dev/shm/gopass* set setlocal noswapfile nobackup noundofile shada="" ]])
+
+vim.filetype.add({
+	filename = {
+		["docker-compose.yml"] = "yaml.docker-compose",
+		["docker-compose.yaml"] = "yaml.docker-compose",
+	},
+})
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
