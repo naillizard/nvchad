a856a5291d5a2de0743685c45245b2c3a5e26bb5
naillizard <1164234+naillizard@users.noreply.github.com>
2024-11-14 07:48:37 +1300

    chore: removed whitespace

--- lua/options.lua
+++ lua/options.lua
@@ -4,9 +4,9 @@ require("nvchad.options")
 vim.g.dap_virtual_text = true -- enable debug adapter protocol (DAP)
 vim.opt.colorcolumn = "80"
 
-vim.cmd([[ autocmd BufNewFile, BufRead *.bicep set filetype=bicep ]])
+vim.cmd([[ autocmd BufNewFile,BufRead *.bicep set filetype=bicep ]])
 
-vim.cmd([[ autocmd BufNewFile, BufRead /dev/shm/gopass* set setlocal noswapfile nobackup noundofile shada="" ]])
+vim.cmd([[ autocmd BufNewFile,BufRead /dev/shm/gopass* set setlocal noswapfile nobackup noundofile shada="" ]])
 
 vim.filetype.add({
 	filename = {
