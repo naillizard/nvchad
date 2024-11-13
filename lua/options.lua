67f0752a3075c83ebb4aa8190c980d85425ecebd
naillizard <1164234+naillizard@users.noreply.github.com>
2024-11-14 08:06:40 +1300

    chore: add auto cmd for bicepparam files

--- lua/options.lua
+++ lua/options.lua
@@ -4,6 +4,20 @@ require("nvchad.options")
 vim.g.dap_virtual_text = true -- enable debug adapter protocol (DAP)
 vim.opt.colorcolumn = "80"
 
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+local general = augroup("General Settings", { clear = true })
+
+autocmd("BufEnter", {
+	pattern = "*.bicepparam",
+	callback = function()
+		vim.bo.filetype = "bicep"
+	end,
+	group = general,
+	desc = "Set bicepparam filetype to bicep",
+})
+
 vim.cmd([[ autocmd BufNewFile,BufRead *.bicep set filetype=bicep ]])
 
 vim.cmd([[ autocmd BufNewFile,BufRead /dev/shm/gopass* set setlocal noswapfile nobackup noundofile shada="" ]])
