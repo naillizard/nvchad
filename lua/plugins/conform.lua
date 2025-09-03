15e92fab60a089d2d21124d9011f6b48d754dbb6
naillizard <diegotdai@gmail.com>
2025-09-04 10:25:03 +1200

    chore(conform): add shfmt mapping and minor formatting cleanup
    This commit introduces a 'shfmt' filetype entry in the formatters_by_ft
    table with the 'beautysh' formatter. It also comments out a specific
    stylua command configuration and updates code style for consistency and
    legibility across the conform.nvim plugin configuration file.

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,39 +1,40 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		event = "BufWritePre", -- uncomment for format on save
-		opts = {
-			formatters_by_ft = {
-				bash = { "beautysh" },
-				-- css = { "prettier" },
-				go = { "golines", "goimports-reviser" },
-				html = { "prettier" },
-				justfile = { "just" },
-				lua = { "stylua" },
-				markdown = { "markdownlint" },
-				nix = { "alejandra" }, -- "alejandra" "nixpkgs_fmt"
-				nushell = { "nufmt" },
-				python = { "autopep8" },
-				rust = { "rustfmt", lsp_format = "fallback" },
-				terraform = { "terraform_fmt" },
-				sh = { "beautysh" },
-				yaml = { "yamlfmt" },
-				zsh = { "beautysh" },
-			},
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    opts = {
+      formatters_by_ft = {
+        bash = { "beautysh" },
+        -- css = { "prettier" },
+        go = { "golines", "goimports-reviser" },
+        html = { "prettier" },
+        justfile = { "just" },
+        lua = { "stylua" },
+        markdown = { "markdownlint" },
+        nix = { "alejandra" }, -- "alejandra" "nixpkgs_fmt"
+        nushell = { "nufmt" },
+        python = { "autopep8" },
+        rust = { "rustfmt", lsp_format = "fallback" },
+        terraform = { "terraform_fmt" },
+        shfmt = { "beautysh" },
+        yaml = { "yamlfmt" },
+        zsh = { "beautysh" },
+      },
 
-			formatters = {
-				stylua = {
-					command = "/home/ronin/.nix-profile/bin/stylua",
-				},
-			},
+      formatters = {
+        stylua = {
+          -- enable on nixos
+          -- command = "/home/ronin/.nix-profile/bin/stylua",
+        },
+      },
 
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				lsp_format = "fallback",
-				timeout_ms = 500,
-			},
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        lsp_format = "fallback",
+        timeout_ms = 500,
+      },
 
-			log_level = vim.log.levels.DEBUG,
-		},
-	},
+      log_level = vim.log.levels.DEBUG,
+    },
+  },
 }
